2f820863fe756f3ebb36e660333a73ae8fdc2a9d
Kutcya <kutcya@foxmail.com>
2024-03-22 22:46:19 +0800

    补充新的打印及注释

--- Void.java
+++ Void.java
@@ -1,7 +1,10 @@
 class Void{
     public static void main(String[] agrs){
         System.out.println("Hello World");
+        System.out.println("Good Bye World");
 
+        // git branch 查看所有分支
+        // git checkout -b 切换并创建分支
         // git init   初始化
         // git status 查看分支目前状态
         // git add    追踪目前的变更--缓存区
