6c36d3047769a2f6a9869248ac5854e18ccaf9ca
Kutcya <kutcya@foxmail.com>
2024-03-22 21:47:16 +0800

    增加了git流程注释

--- Void.java
+++ Void.java
@@ -1,5 +1,13 @@
 class Void{
     public static void main(String[] agrs){
         System.out.println("Hello World");
+
+        // git init   初始化
+        // git status 查看分支目前状态
+        // git add    追踪目前的变更--缓存区
+        // git commit 提交目前的变更到本地仓库--隐藏的储存区
+        // git push   推送本地变更到远程仓库
+        // git remote 管理远程仓库分支
+        // git pull
     }
 }
